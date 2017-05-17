fd4d76ec55cc6bb2f5a23699bffff75f0a15a198
t3kt <tommy.etkin@gmail.com>
2017-05-16 19:11:30 -0700

    added python schema parsing tool and fixed bugs in the legacy json formatter

--- python/tctrl/legacy_json.py
+++ python/tctrl/legacy_json.py
@@ -1,6 +1,12 @@
 import tctrlgen.tctrl_schema_pb2 as pb
+import google.protobuf.struct_pb2 as struct_pb
+import google.protobuf.wrappers_pb2 as wrap_pb
+import google.protobuf.text_format as text_format
 
 class LegacyJsonParser:
+
+    def parseAppSpec(self, obj):
+        raise NotImplemented()
 
     @staticmethod
     def parseParamOption(obj):
@@ -66,12 +72,12 @@ class LegacyJsonBuilder:
             'key': spec.key,
             'label': spec.label,
             'path': spec.path,
-            'default': spec.defaultVal,
-            'value': spec.value,
-            'minLimit': spec.minLimit,
-            'maxLimit': spec.maxLimit,
-            'minNorm': spec.minNorm,
-            'maxNorm': spec.maxNorm,
+            'default': self.convertValue(spec.defaultVal),
+            'value': self.convertValue(spec.value),
+            'minLimit': self.convertValue(spec.minLimit),
+            'maxLimit': self.convertValue(spec.maxLimit),
+            'minNorm': self.convertValue(spec.minNorm),
+            'maxNorm': self.convertValue(spec.maxNorm),
         })
 
     def paramToObj(self, spec: pb.ParamSpec):
@@ -81,21 +87,21 @@ class LegacyJsonBuilder:
             'path': spec.path,
             'style': spec.style,
             'group': spec.group,
-            'tags': spec.tag,
+            'tags': spec.tag[:] if spec.tag else None,
             'help': spec.help,
             'offHelp': spec.offHelp,
             'buttonText': spec.buttonText,
             'buttonOffText': spec.buttonOffText,
             'type': _TypeToString(spec.type),
             'otherType': spec.otherType,
-            'default': spec.defaultVal,
-            'value': spec.value,
-            'valueIndex': spec.valueIndex,
-            'minLimit': spec.minLimit,
-            'maxLimit': spec.maxLimit,
-            'minNorm': spec.minNorm,
-            'maxNorm': spec.maxNorm,
-            'options': [self.paramOptionToObj(o) for o in spec.options] if spec.options else None,
+            'default': self.convertValue(spec.defaultVal),
+            'value': self.convertValue(spec.value),
+            'valueIndex': self.convertInt32Value(spec.valueIndex),
+            'minLimit': self.convertValue(spec.minLimit),
+            'maxLimit': self.convertValue(spec.maxLimit),
+            'minNorm': self.convertValue(spec.minNorm),
+            'maxNorm': self.convertValue(spec.maxNorm),
+            'options': [self.paramOptionToObj(o) for o in spec.option] if spec.option else None,
             'optionsList': spec.optionListKey,
             'parts': [self.paramPartSpecToObj(o) for o in spec.part] if spec.part else None,
         })
@@ -115,7 +121,7 @@ class LegacyJsonBuilder:
             'path': spec.path,
             'moduleType': spec.moduleType,
             'group': spec.group,
-            'tags': spec.tag,
+            'tags': spec.tag[:] if spec.tag else None,
             'paramGroups': [self.groupInfoToObj(o) for o in spec.paramGroup] if spec.paramGroup else None,
             'params': [self.paramToObj(o) for o in spec.param] if spec.param else None,
             'childGroups': [self.groupInfoToObj(o) for o in spec.childGroup] if spec.childGroup else None,
@@ -135,7 +141,7 @@ class LegacyJsonBuilder:
         return _CleanDict({
             'key': spec.key,
             'label': spec.label,
-            'tags': spec.tag,
+            'tags': spec.tag[:] if spec.tag else None,
         })
 
     def appSpecToObj(self, spec: pb.AppSpec):
@@ -143,7 +149,7 @@ class LegacyJsonBuilder:
             'key': spec.key,
             'label': spec.label,
             'path': spec.path,
-            'tags': spec.tag,
+            'tags': spec.tag[:] if spec.tag else None,
             'description': spec.description,
             'moduleTypes': [self.moduleTypeSpecToObj(o) for o in spec.moduleType] if spec.moduleType else None,
             'optionLists': [self.optionListToObj(o) for o in spec.optionList] if spec.optionList else None,
@@ -151,6 +157,26 @@ class LegacyJsonBuilder:
             'children': [self.moduleSpecToObj(o) for o in spec.childModule] if spec.childModule else None,
             'connections': [self.connectionInfoToObj(o) for o in spec.connection] if spec.connection else None,
         })
+
+    @staticmethod
+    def convertValue(value: struct_pb.Value):
+        if value is None or value.WhichOneof('kind') is None:
+            return None
+        if value.HasField('null_value'):
+            return None
+        if value.HasField('bool_value'):
+            return value.bool_value
+        if value.HasField('number_value'):
+            return value.number_value
+        if value.HasField('string_value'):
+            return value.string_value
+        raise Exception('Unsupported value type: ' + text_format.MessageToString(value))
+
+    @staticmethod
+    def convertInt32Value(value: wrap_pb.Int32Value):
+        if value is None:
+            return None
+        return value.value
 
 def _TypeToString(paramtype: pb.ParamType):
     if paramtype is None:
---
+++ python/tctrl/schema_tool.py
@@ -0,0 +1,40 @@
+import google.protobuf.json_format as pbjson
+import json
+import tctrlgen.tctrl_schema_pb2 as pb
+import sys
+import tctrl.legacy_json
+
+def main():
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('infile', metavar='F')
+    parser.add_argument('-i', '--infmt', dest='infmt', default='json')
+    parser.add_argument('-o', '--outfmt', dest='outfmt', default='json')
+
+    args = parser.parse_args()
+
+    with open(args.infile, 'r') as infile:
+        rawinput = infile.read()
+    infmt = args.infmt
+    if infmt == 'pbjson':
+        appspec = pbjson.Parse(rawinput, pb.AppSpec())
+    elif infmt == 'json':
+        inputdict = json.loads(rawinput)
+        appspec = tctrl.legacy_json.LegacyJsonParser().parseAppSpec(inputdict)
+    else:
+        parser.print_usage()
+        raise Exception('Invalid input format: ' + infmt)
+
+    outfmt = args.outfmt
+    if outfmt == 'pbjson':
+        outdict = pbjson.MessageToDict(appspec)
+    elif outfmt == 'json':
+        outdict = tctrl.legacy_json.LegacyJsonBuilder().appSpecToObj(appspec)
+    else:
+        parser.print_usage()
+        raise Exception('Invalid output format: ' + outfmt)
+    json.dump(outdict, sys.stdout, sort_keys=True, indent='  ')
+
+if __name__ == '__main__':
+    main()
