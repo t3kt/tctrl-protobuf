4e35b516b5d769625c51d1dae69edbf87c78b777
t3kt <tommy.etkin@gmail.com>
2017-06-04 20:06:23 -0700

    starting on csharp stuff

---
+++ win/TctrlCore/Schema/SchemaNode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Google.Protobuf;
+
+namespace Tctrl.Core.Schema
+{
+    abstract class SchemaNode<S> where S : IMessage
+    {
+        public SchemaNode(S spec) => this.Spec = spec;
+
+        public S Spec { get; private set; }
+
+        public abstract String getKey();
+
+        public abstract String getLabel();
+
+        public abstract String getPath();
+    }
+}
